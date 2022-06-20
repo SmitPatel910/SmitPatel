5c93dcac38de460d8b11af54bca654665fd66da2
SmitPatel910 <patel9102000@gmail.com>
2022-06-21 02:58:36 +0530

    Updates

---
+++ precache-manifest.f4817af69b4afd56a65e8e27e65bc7d8.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92302a4aa044cc1349da187d2d6427b",
+    "url": "/SmitPatel/index.html"
+  },
+  {
+    "revision": "f961d263d7aa4c8f3c50",
+    "url": "/SmitPatel/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "14f974a167e7529e8343",
+    "url": "/SmitPatel/static/css/main.58a5c203.chunk.css"
+  },
+  {
+    "revision": "8ebbe8dcac5163fc863b",
+    "url": "/SmitPatel/static/js/2.d74657bb.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/SmitPatel/static/js/2.d74657bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f961d263d7aa4c8f3c50",
+    "url": "/SmitPatel/static/js/3.445946d1.chunk.js"
+  },
+  {
+    "revision": "14f974a167e7529e8343",
+    "url": "/SmitPatel/static/js/main.339faaa3.chunk.js"
+  },
+  {
+    "revision": "3e5487c5109d0cb7e70c",
+    "url": "/SmitPatel/static/js/runtime-main.c5905f65.js"
+  },
+  {
+    "revision": "c389994d42c968147d5145234e61c0e1",
+    "url": "/SmitPatel/static/media/ACM.c389994d.jpg"
+  },
+  {
+    "revision": "d9c9831ed890f924ffc2f207f4f2b044",
+    "url": "/SmitPatel/static/media/ASU.d9c9831e.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/SmitPatel/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "f445c4e258dcf48bbad73095e43be67a",
+    "url": "/SmitPatel/static/media/Appointment_dark.f445c4e2.png"
+  },
+  {
+    "revision": "c6cfceeca4d75c6ad12a539004073397",
+    "url": "/SmitPatel/static/media/Appointment_light.c6cfceec.png"
+  },
+  {
+    "revision": "0593116f113d6895a625ab001e095054",
+    "url": "/SmitPatel/static/media/AsthaTechnology.0593116f.jpg"
+  },
+  {
+    "revision": "b258029618d1e3d72862e7c52f083033",
+    "url": "/SmitPatel/static/media/DiskScheduling.b2580296.webp"
+  },
+  {
+    "revision": "2f864031437719988c521ce90f28f181",
+    "url": "/SmitPatel/static/media/Gyanmanjari.2f864031.jpg"
+  },
+  {
+    "revision": "bc0f8bd699f68da73afecfd0e89cc945",
+    "url": "/SmitPatel/static/media/Hackathon2.bc0f8bd6.jpg"
+  },
+  {
+    "revision": "44140b1d9c76886da767382a718a0067",
+    "url": "/SmitPatel/static/media/HomepagePlugin.44140b1d.png"
+  },
+  {
+    "revision": "4b6ce28db8bd528163a478c16b054986",
+    "url": "/SmitPatel/static/media/Income-Expense-Analysis.4b6ce28d.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/SmitPatel/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "a46afc986de2a8bca4d8e4416c54c70e",
+    "url": "/SmitPatel/static/media/PDPU.a46afc98.jpg"
+  },
+  {
+    "revision": "77c8b565c6447796757fee6ad14d1531",
+    "url": "/SmitPatel/static/media/ParkingSpaceCounter.77c8b565.jpg"
+  },
+  {
+    "revision": "caad72517b96ce148ec70d18d498168b",
+    "url": "/SmitPatel/static/media/SLUU.caad7251.png"
+  },
+  {
+    "revision": "3c7e39704ebd25171b8f9eab564476cf",
+    "url": "/SmitPatel/static/media/Smit22.3c7e3970.png"
+  },
+  {
+    "revision": "f86f098f949338e94e40c469327b2289",
+    "url": "/SmitPatel/static/media/Smit3.f86f098f.png"
+  },
+  {
+    "revision": "8a8d9e53b04d723667c683897558c8d0",
+    "url": "/SmitPatel/static/media/SortingAlgo.8a8d9e53.png"
+  },
+  {
+    "revision": "c2dcf6c015ec9fb272d460b602c62711",
+    "url": "/SmitPatel/static/media/Spotify.c2dcf6c0.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/SmitPatel/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/SmitPatel/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "5a970482be5640c2e44866a4aeed36f3",
+    "url": "/SmitPatel/static/media/verzeo.5a970482.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad8d192732032cc07fa77181437c1743.js"
+  "/SmitPatel/precache-manifest.f4817af69b4afd56a65e8e27e65bc7d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SmitPatel/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
